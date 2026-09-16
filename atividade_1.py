f5e44a4faa250b7e6d4324f10b2e87e20f28bbe9
pedrotosetti-ui <pedro.tosetti@eaportal.org>
2026-09-15 22:11:08 -0300

    atividade_1.py

---
+++ atividade_1.py
@@ -0,0 +1,9 @@
+primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+numero = 0
+for i in primos:
+
+    if i < 67:
+
+        numero += 1
+
+print(f"Existem {numero} números primos menores que 67")
